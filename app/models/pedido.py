42c4686d441e037ee6b3e636877da9ead0a69211
Ariel Amaral <amaral.ariel95@gmail.com>
2023-06-26 19:02:20 -0300

    Correção de relacionamento de models com o pedido e item_pedido

--- app/models/pedido.py
+++ app/models/pedido.py
@@ -4,7 +4,7 @@ class Pedido(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     valor_total = db.Column(db.Float, nullable=False)
 
-    itens = db.relationship('ItemPedido', backref='pedido', lazy=True)
+    itens = db.relationship('Item_Pedido', backref='pedido', lazy=True)
 
     def __repr__(self):
         return f"<Pedido {self.id}>"
