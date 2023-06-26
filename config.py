2828aacabfe1df691a134276e789b93cc8ae8dd3
Ariel Amaral <amaral.ariel95@gmail.com>
2023-06-25 21:00:37 -0300

    Revisão de configuração de banco de dados localmente

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:postgres@db/tech_challenge'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'your-secret-key'
