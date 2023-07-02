ddf671772abd0ce83adeef06eef917350f7be0f0
Ariel Amaral <amaral.ariel95@gmail.com>
2023-07-02 19:50:11 -0300

    Correção do init para Models e Services do Pagamento

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,3 +2,4 @@ from app.models.cliente import Cliente
 from app.models.produto import Produto
 from app.models.pedido import Pedido
 from app.models.item_pedido import Item_Pedido
+from app.models.pagamento import Pagamento
